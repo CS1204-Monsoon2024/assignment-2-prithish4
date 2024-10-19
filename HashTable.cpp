e8273806fc29b3623d4e65fb8d3831fcdcfdf16e
prithish4 <143925140+prithish4@users.noreply.github.com>
2024-10-19 23:42:23 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -66,6 +66,14 @@ public:
 
     // Insert operation
     void insert(int key) {
+	if (num_elements == current_size)
+	{
+		cout << "Max probing limit reached!" << endl;
+	}
+	if (search(key) != -1)
+	{
+		cout << "Duplicate key insertion is not allowed" << endl;
+	}
         check_load_factor(); // Check if rehashing is needed based on load factor
 
         int index = hash(key);
@@ -108,6 +116,7 @@ public:
             }
             i++;
         }
+	cout << "Duplicate key insertion is not allowed" << endl;
     }
 
     // Function to print the current hash table
